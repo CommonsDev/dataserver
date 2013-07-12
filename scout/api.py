fbf326d7f367ed5340d2f88e28b5d3322f545379
Guillaume Libersat <glibersat@sigill.org>
2013-07-12 18:29:50 +0200

    secure api, set username from current user when creating point

--- scout/api.py
+++ scout/api.py
@@ -4,6 +4,8 @@ from tastypie.authentication import ApiKeyAuthentication
 from tastypie.contrib.gis.resources import ModelResource as GeoModelResource
 from tastypie.resources import ModelResource
 
+from django.contrib.auth.models import User
+
 from .models import Map, TileLayer, Marker
 
 from accounts.api import ProfileResource
@@ -12,7 +14,8 @@ class MapResource(GeoModelResource):
     class Meta:
         queryset = Map.objects.all()
         resource_name = 'map'
-        authorization = DjangoAuthorization()        
+        authorization = DjangoAuthorization()
+        detail_uri_name = 'slug'        
 
     tile_layers = fields.ToManyField('scout.api.TileLayerResource', 'tilelayers', full=True)
 
@@ -30,10 +33,18 @@ class MarkerResource(GeoModelResource):
     class Meta:
         queryset = Marker.objects.all()
         resource_name = 'marker'
-        authorization = Authorization() # Unsecure
+        authentication = ApiKeyAuthentication()
+        authorization = DjangoAuthorization()
     
     tile_layer = fields.ToOneField(TileLayerResource, 'tile_layer')
     created_by = fields.ToOneField(ProfileResource, 'created_by', full=True)
+
+    def hydrate(self, bundle, request=None):
+        if not bundle.obj.pk:
+            user = User.objects.get(pk=bundle.request.user.id)
+            bundle.data['created_by'] = {'pk': user.get_profile().pk}
+            
+        return bundle     
         
     
     
