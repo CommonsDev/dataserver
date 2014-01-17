c852ab204ccd381a60f2cf233364b960cc45154d
Gup <gup@sd-45990.dedibox.fr>
2014-01-17 17:31:55 +0100

    merged

--- gup/settings.py
+++ gup/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'gup',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'gup',
-        'PASSWORD': 'gup',
+        'PASSWORD': 'gupgup',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.s
     }
@@ -69,7 +69,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = './'
+STATIC_ROOT = '/home/gup/envs/dataserver-env/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
