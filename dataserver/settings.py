f1e4eb5397696f611cfab94bc2e47c76b8efe1af
Guillaume Libersat <glibersat@sigill.org>
2014-10-08 22:13:19 +0200

    update settings

--- dataserver/settings.py
+++ dataserver/settings.py
@@ -1,19 +1,8 @@
 # Django settings for dataserver project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
-
 from site_settings import *
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['YOUR_HOST']
+MANAGERS = ADMINS
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
