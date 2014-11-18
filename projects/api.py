08ccf104554c4e2b5702fca9e2901271f18ccd5f
Alban <alban.tiberghien@gmail.com>
2014-11-18 14:27:49 +0100

    Add project progress related resources

--- projects/api.py
+++ projects/api.py
@@ -1,15 +1,39 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
 
-from .models import Project
+from .models import Project, ProjectProgressRange, ProjectProgress
 
 from scout.api import PostalAddressResource
 from dataserver.authentication import AnonymousApiKeyAuthentication
 from tastypie.authorization import DjangoAuthorization
+from tastypie.constants import ALL_WITH_RELATIONS
+
+class ProjectProgressRangeResource(ModelResource):
+    class Meta :
+        queryset = ProjectProgressRange.objects.all()
+        allowed_methods = ['get']
+    
+        filtering = {
+            "slug": ('exact',),
+        }
+        
+class ProjectProgressResource(ModelResource):
+    range = fields.ToOneField(ProjectProgressRangeResource, "progress_range")
+    
+    class Meta:
+        queryset = ProjectProgress.objects.all()
+        allowed_methods = ['get']
+        always_return_data = True
+    
+        filtering = {
+            "range": ALL_WITH_RELATIONS,
+        }
+        
 
 class ProjectResource(ModelResource):
-    location = fields.ToOneField(PostalAddressResource, 'location', null=True, blank=True)
-     
+    location = fields.ToOneField(PostalAddressResource, 'location', null=True, blank=True, full=True)
+    progress = fields.ToOneField(ProjectProgressResource, 'progress', null=True, blank=True, full=True)
+    
     class Meta:
         queryset = Project.objects.all()
         allowed_methods = ['get', 'post', 'put', 'patch']
--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,19 @@ from autoslug.fields import AutoSlugField
 from taggit.managers import TaggableManager
 from scout.models import PostalAddress
 
+class ProjectProgressRange(models.Model):
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(unique=True, populate_from="name", always_update=True)
+    
+class ProjectProgress(models.Model):
+    progress_range = models.ForeignKey(ProjectProgressRange)
+    order = models.PositiveIntegerField(default=0)
+    label = models.CharField(max_length=30)
+    icon = models.ImageField(upload_to='progress_icons')
+    
+    class Meta:
+        ordering  = ['order',]
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     slug = AutoSlugField(unique=True)
@@ -13,6 +26,7 @@ class Project(models.Model):
     website = models.URLField(null=True, blank=True)
     begin_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
+    progress = models.ForeignKey(ProjectProgress, null=True, blank=True)
     
     def __unicode__(self):
         return self.title
