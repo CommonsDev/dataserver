662dcfc66646dbb02aa60f069f4ab26be2c8c57b
SimonSarazin <simon.sarazin@gmail.com>
2014-11-26 18:28:01 +0100

    Updated tools to get categoy

--- projecttools/admin.py
+++ projecttools/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import ProjectTools
+from .models import ToolCategory, ProjectTools
 
-class ProjectAdmin(admin.ModelAdmin):
-    pass
 
-admin.site.register(ProjectTools, ProjectAdmin)
+admin.site.register(ProjectTools)
+admin.site.register(ToolCategory)
--- projecttools/models.py
+++ projecttools/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from projects.models import Project
 
-class ProjectTools(models.Model):
-    project = models.OneToOneField(Project)
-    contribution_meeting = models.TextField(blank=True)
-    contribution_onlinemeeting = models.TextField(blank=True)
-    def __unicode__(self):
-        return self.contribution_meeting
+class ToolCategory(models.Model):
+    toolcategory = models.CharField(max_length=200)
+    description = models.CharField(verbose_name=("description"), max_length=500, null=True, blank=True)
+    example = models.TextField()
+    website = models.URLField(null=True, blank=True)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return unicode(self.toolcategory)	
     
+class ProjectTools(models.Model):
+    project = models.OneToOneField(Project, related_name='project_category')
+    category = models.OneToOneField(ToolCategory)
+    projecttool = models.TextField(blank=True, verbose_name=('tool description'))
+    website = models.URLField(null=True, blank=True, verbose_name=('link to the tool'))
+
+    def __unicode__(self):
+        return self.projecttool
