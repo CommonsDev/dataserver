9d816efc62ab4047501e5d77c0471090ce5e279e
SimonSarazin <simon.sarazin@gmail.com>
2014-11-30 18:25:00 +0100

    Update ProjetTool Api

--- projecttool/api.py
+++ projecttool/api.py
@@ -7,7 +7,7 @@ class ProjectToolResource(ModelResource):
     class Meta:
         queryset = ProjectTool.objects.all()
         allowed_methods = ['get', 'post', 'patch']
-        resource_name = 'projecttool'
+        resource_name = 'project/tools'
         authorization = Authorization()
 
     
