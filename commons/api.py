43dbbbaddba2d1a4c9134ed4dba8fd06082d46c1
SimonSarazin <simon.sarazin@gmail.com>
2014-12-19 19:46:22 +0100

    Updated commons et projecttool app API

--- commons/api.py
+++ commons/api.py
@@ -3,13 +3,6 @@ from tastypie.authorization import Authorization
 from tastypie import fields
 from .models import Project, Usage, Pertinence
 
-class UsageResource(ModelResource):
-    class Meta:
-        queryset = Usage.objects.all()
-        allowed_methods = ['get', 'post', 'patch']
-        resource_name = 'project/commons/usage'
-        authorization = Authorization()
-
 class PertinenceResource(ModelResource):
     class Meta:
         queryset = Pertinence.objects.all()
@@ -17,5 +10,15 @@ class PertinenceResource(ModelResource):
         resource_name = 'project/commons/pertinence'
         authorization = Authorization()
 
-    project = fields.ForeignKey('projects.api.ProjectResource', 'project', full=True)
-    usage = fields.ForeignKey('commons.api.UsageResource', 'usage', full=True)
+    project = fields.ForeignKey('projects.api.ProjectResource', 'project', null=True, blank=True, full=True)
+    usage = fields.ForeignKey('commons.api.UsageResource', 'usage', full=True, null=True, blank=True)
+
+
+class UsageResource(ModelResource):
+    class Meta:
+        queryset = Usage.objects.all()
+        allowed_methods = ['get', 'post', 'patch']
+        resource_name = 'project/commons/usage'
+        authorization = Authorization()
+
+    project = fields.ToManyField('projects.api.ProjectResource', 'project', null=True, blank=True, full=True)
