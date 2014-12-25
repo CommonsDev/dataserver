83db9f75c5c54f2262a0f587a91251283887d66a
Guillaume Libersat <glibersat@sigill.org>
2014-12-25 22:42:33 +0100

    Revert "Updated commons et projecttool app API"
    This reverts commit 43dbbbaddba2d1a4c9134ed4dba8fd06082d46c1.

--- commons/api.py
+++ commons/api.py
@@ -3,17 +3,6 @@ from tastypie.authorization import Authorization
 from tastypie import fields
 from .models import Project, Usage, Pertinence
 
-class PertinenceResource(ModelResource):
-    class Meta:
-        queryset = Pertinence.objects.all()
-        allowed_methods = ['get', 'post', 'patch']
-        resource_name = 'project/commons/pertinence'
-        authorization = Authorization()
-
-    project = fields.ForeignKey('projects.api.ProjectResource', 'project', null=True, blank=True, full=True)
-    usage = fields.ForeignKey('commons.api.UsageResource', 'usage', full=True, null=True, blank=True)
-
-
 class UsageResource(ModelResource):
     class Meta:
         queryset = Usage.objects.all()
@@ -21,4 +10,12 @@ class UsageResource(ModelResource):
         resource_name = 'project/commons/usage'
         authorization = Authorization()
 
-    project = fields.ToManyField('projects.api.ProjectResource', 'project', null=True, blank=True, full=True)
+class PertinenceResource(ModelResource):
+    class Meta:
+        queryset = Pertinence.objects.all()
+        allowed_methods = ['get', 'post', 'patch']
+        resource_name = 'project/commons/pertinence'
+        authorization = Authorization()
+
+    project = fields.ForeignKey('projects.api.ProjectResource', 'project', full=True)
+    usage = fields.ForeignKey('commons.api.UsageResource', 'usage', full=True)
--- projecttool/api.py
+++ projecttool/api.py
@@ -4,17 +4,7 @@ from tastypie.resources import ModelResource
 
 from dataserver.authentication import AnonymousApiKeyAuthentication
 
-from .models import Project, ProjectTool, ToolCategory
-
-
-class ToolCategoryResource(ModelResource):
-    class Meta:
-        queryset = ToolCategory.objects.all()
-        allowed_methods = ['get', 'post', 'patch']
-        resource_name = 'project/tool/category'
-        authentication = AnonymousApiKeyAuthentication()
-        authorization = Authorization()
-    
+from .models import Project, ProjectTool
 
 class ProjectToolResource(ModelResource):
     class Meta:
@@ -23,5 +13,3 @@ class ProjectToolResource(ModelResource):
         resource_name = 'project/tool'
         authentication = AnonymousApiKeyAuthentication()
         authorization = Authorization()
-    
-    category = fields.ToOneField(ToolCategoryResource, 'category', null=True, blank=True, full=True)
