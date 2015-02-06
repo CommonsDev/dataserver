1f5f7e6e55b82e8105027641144bb6d419c6f485
Alban <alban.tiberghien@gmail.com>
2015-02-06 15:29:18 +0100

    Improve taggit in order to allow resource inheriance

--- graffiti/api.py
+++ graffiti/api.py
@@ -37,22 +37,23 @@ class TaggedItemResource(ModelResource):
         authorization = DjangoAuthorization()
         default_format = "application/json"
         filtering = {
-            "tag" : ALL_WITH_RELATIONS
+            "tag" : ALL_WITH_RELATIONS,
+            "object_id" : ['exact', ]
         }
         always_return_data = True
 
     def prepend_urls(self):
         return [
-           url(r"^(?P<resource_name>%s)/(?P<object_type>\w+?)/(?P<object_id>\d+?)%s$" % (self._meta.resource_name, trailing_slash()),
+           url(r"^(?P<resource_name>%s)/(?P<content_type>\w+?)/(?P<object_id>\d+?)%s$" % (self._meta.resource_name, trailing_slash()),
                self.wrap_view('dispatch_list'),
                name="api_dispatch_list"),
-            url(r"^(?P<resource_name>%s)/(?P<object_type>\w+?)/(?P<object_id>\d+?)/similars%s$" % (self._meta.resource_name, trailing_slash()),
+            url(r"^(?P<resource_name>%s)/(?P<content_type>\w+?)/(?P<object_id>\d+?)/similars%s$" % (self._meta.resource_name, trailing_slash()),
                self.wrap_view('get_similars'),
                name="api_get_similars"),
         ]
 
     def get_similars(self, request, **kwargs):
-        obj = ContentType.objects.get(model=kwargs.pop('object_type')).get_object_for_this_type(id=kwargs["object_id"])
+        obj = ContentType.objects.get(model=kwargs.pop('content_type')).get_object_for_this_type(id=kwargs["object_id"])
         return HttpResponse(json.dumps([{'id' : o.id, 'type' : ContentType.objects.get_for_model(o).model} for o in obj.tags.similar_objects()]))
 
     def dispatch_list(self, request, **kwargs):
@@ -60,13 +61,19 @@ class TaggedItemResource(ModelResource):
         self.is_authenticated(request)
         self.throttle_check(request)
 
-        if 'object_type' in kwargs and 'object_id' in kwargs and request.method=="POST":
+
+        if 'content_type' in kwargs and 'object_id' in kwargs and request.method=="POST":
             data = json.loads(request.body)
             if 'tag' in data:
                 tag_obj, created = Tag.objects.get_or_create(name=data['tag'], slug=slugify(data['tag']))
-                tagged_item, created = TaggedItem.objects.get_or_create(tag=tag_obj,
-                                                 content_type=ContentType.objects.get(model=kwargs['object_type']),
-                                                 object_id=kwargs['object_id'])
+
+                params = kwargs.copy()
+                del params['resource_name']
+                del params['api_name']
+                params['tag'] = tag_obj
+                params['content_type'] = ContentType.objects.get(model=kwargs['content_type'])
+
+                tagged_item, created = self._meta.queryset.model.objects.get_or_create(**params)
 
                 bundle = self.build_bundle(obj=tagged_item, request=request)
                 bundle = self.full_dehydrate(bundle)
