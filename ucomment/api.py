e84d97dcce08411879a78b0fdbdd4d4aec631344
Freddy Limpens <freddy.limpens@gmail.com>
2015-02-18 11:11:48 +0100

    added flag feature + commentflag admin that links to comment change admin pages

--- ucomment/api.py
+++ ucomment/api.py
@@ -1,14 +1,19 @@
 import json
 import datetime
 
-from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls import url
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import get_current_site
+from django.core import urlresolvers
+from django.core.mail import send_mail
 from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.utils.text import slugify
 
-from django_comments import Comment
+from django_comments.models import Comment, CommentFlag
+from django_comments.views.moderation import perform_flag
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization, DjangoAuthorization
 from tastypie import fields
@@ -20,10 +25,17 @@ from dataserver.authentication import AnonymousApiKeyAuthentication
 from accounts.api import ProfileResource, UserResource
 from accounts.models import Profile
 
+class CommentFlagResource(ModelResource):
+    flag=fields.CharField(attribute='flag')
+    
+    class Meta:
+        queryset = CommentFlag.objects.all()
+
+
 class CommentResource(ModelResource):
     comment = fields.CharField(attribute='comment')
     user = fields.ToOneField(UserResource, 'user', full='true')
-
+    flags = fields.ToManyField(CommentFlagResource, 'flags', full='false', null='true')
     class Meta:
         queryset = Comment.objects.all()
         resource_name = 'comment'
@@ -32,7 +44,7 @@ class CommentResource(ModelResource):
             "content_type": "exact",
             "object_pk": "exact"
         }
-        allowed_methods = ['get', 'post']
+        allowed_methods = ['get', 'post', 'delete']
         always_return_data = True
         authentication = AnonymousApiKeyAuthentication()
         authorization = DjangoAuthorization()
@@ -43,7 +55,36 @@ class CommentResource(ModelResource):
            url(r"^(?P<resource_name>%s)/(?P<content_type>\w+?)/(?P<object_pk>\d+?)%s$" % (self._meta.resource_name, trailing_slash()),
                self.wrap_view('dispatch_list'),
                name="api_dispatch_list"),
-                   ]
+           url(r"^(?P<resource_name>%s)/(?P<comment_id>\d+?)/flag%s$" % (self._meta.resource_name, trailing_slash()),
+               self.wrap_view('flag_ucomment'),
+               name="api_flag")
+            ]
+
+    def flag_ucomment(self, request, **kwargs):
+        """
+        Flags a comment on POST request only using method from django_comments view (to bypass csrf protection)
+        
+        """
+        self.method_check(request, allowed=['post'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
+        # Flag on POST
+        if request.method == 'POST':
+            comment = get_object_or_404(Comment, pk=kwargs['comment_id'], site__pk=settings.SITE_ID)
+            perform_flag(request, comment)
+            
+            # FIXME : basic email sending
+            change_url = urlresolvers.reverse('admin:django_comments_comment_change', args=(comment.id,))
+            host = request.get_host()
+            recipient_list = [manager_tuple[1] for manager_tuple in settings.MANAGERS]
+            subject = '[%s] Un commentaire abusif signale ' % (get_current_site(request).name)
+            message = "Un commentaire abusif a ete signale. Vous pouvez le moderer et le supprimer si besoin a cette adresse %s%s" % (host, change_url)
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=True)
+
+            return HttpResponse("Comment flaged")
+
+
 
     def dispatch_list(self, request, **kwargs):
         self.method_check(request, allowed=['get', 'post'])
