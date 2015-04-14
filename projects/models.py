4673f2fb4ff9c6b6f0dd6a69ff4009259306271c
Olivier Cortès <oc@1flow.io>
2015-04-14 16:46:16 +0200

    flake8 happyness.

--- projects/models.py
+++ projects/models.py
@@ -1,19 +1,31 @@
+""" Projects and related models. """
+
 from django.db import models
 from autoslug.fields import AutoSlugField
 from taggit.managers import TaggableManager
 from scout.models import Place
 from accounts.models import Profile
-from django.db.models.signals import post_save
-from django.dispatch.dispatcher import receiver
+# from django.db.models.signals import post_save
+# from django.dispatch.dispatcher import receiver
+
 
 class ProjectProgressRange(models.Model):
+
+    """ A project progress range. """
+
     name = models.CharField(max_length=100)
     slug = AutoSlugField(unique=True, populate_from="name", always_update=True)
 
     def __unicode__(self):
+        """ pep257, you know I love you. """
+
         return u"%s" % self.name
 
+
 class ProjectProgress(models.Model):
+
+    """ A project progress. """
+
     progress_range = models.ForeignKey(ProjectProgressRange)
     order = models.PositiveIntegerField(default=0)
     label = models.CharField(max_length=30)
@@ -21,15 +33,18 @@ class ProjectProgress(models.Model):
     icon = models.ImageField(upload_to='progress_icons')
 
     class Meta:
-        ordering  = ['order',]
+        ordering  = ['order', ]
 
     def __unicode__(self):
+        """ pep257, you know I love you. """
+
         return u"%s - %s - %s" % (self.progress_range, self.order, self.label)
 
+
 class Project(models.Model):
-    """
-    A project is any idea you can document
-    """
+
+    """ A project is any idea you can document. """
+
     title = models.CharField(max_length=100)
     slug = AutoSlugField(unique=True)
     baseline = models.CharField(max_length=250, null=True, blank=True)
@@ -43,9 +58,18 @@ class Project(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
+        """ pep257, you know I love you. """
         return self.title
+
 
 # XXX/TODO: obsolete
 class ProjectTeam(models.Model):
+
+    """ A project team.
+
+    .. todo:: this model is probably obsolete.
+        Someones knowing the truth checks it?
+    """
+
     project = models.ForeignKey(Project)
     members = models.ManyToManyField(Profile)
