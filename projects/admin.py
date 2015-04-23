f9c36972d34fe141986e85f9ea59e894ef3b2dda
Olivier Cortès <oc@1flow.io>
2015-04-23 21:42:45 +0200

    flake8 happyness again.

--- projects/admin.py
+++ projects/admin.py
@@ -5,14 +5,17 @@ from simple_history.admin import SimpleHistoryAdmin
 
 from .models import Project, ProjectProgress, ProjectProgressRange
 
+
 class ProjectProgressInline(StackedInline):
     model = ProjectProgress
     extra = 0
     min_num = 2
     can_delete = False
 
+
 class ProjectProgressRangeAdmin(admin.ModelAdmin):
     inlines = [ProjectProgressInline]
+
 
 class ProjectAdmin(SimpleHistoryAdmin):
     pass
