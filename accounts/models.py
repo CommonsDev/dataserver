4e971e6203c61bbf65e17015c12fef6a03ae39ef
Olivier Cortès <oc@1flow.io>
2015-05-06 11:20:04 +0200

    flake8 happyness.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+""" User accounts related models. """
+
 from django.conf import settings
 from django.contrib.auth.models import User, Group
 from django.contrib.contenttypes import generic
@@ -15,6 +17,9 @@ from userena.utils import get_profile_model
 
 
 class Profile(UserenaBaseProfile):
+
+    """ Link Profile to user with a O2O. """
+
     user = models.OneToOneField(User,
                                 unique=True,
                                 verbose_name=_('user'),
@@ -22,18 +27,26 @@ class Profile(UserenaBaseProfile):
 
     @property
     def username(self):
+        """ Return the user account username. """
+
         return self.user.username
 
+
 class ObjectProfileLink(models.Model):
+
+    """ Record a link between a user profile and any object (eg. Project…).
+
+    With different levels of implication (eg. Project Team Member, Project
+    follower, Project Resource member) and a free field for detail (eg.
+    as role). Validation process is covered by an aditional boolean field.
     """
-    Generic class to record a link between a user profile and an object (Project e.g.)
-    with different levels of implication (Project Team Member, Project follower, Project Resource member e.g)
-    and a free field for detail (as role e.g). Validation process is covered by an aditional boolean field.
-    """
+
     content_type = models.ForeignKey(ContentType)
     object_id = models.TextField(_('object_id'))
-    content_object = generic.GenericForeignKey('content_type', 'object_id', _("Linked object"))
-    profile = models.ForeignKey(Profile, verbose_name = _("Linked user profile"))
+    content_object = generic.GenericForeignKey('content_type', 'object_id',
+                                               _("Linked object"))
+    profile = models.ForeignKey(Profile,
+                                verbose_name=_("Linked user profile"))
     level = models.IntegerField(_("Implication level of the link"))
     detail = models.CharField(max_length=200, blank=True)
     isValidated = models.BooleanField(default=False)
@@ -41,24 +54,30 @@ class ObjectProfileLink(models.Model):
 
 @receiver(post_save, sender=User)
 def create_profile_on_user_signup(sender, created, instance, **kwargs):
+    """ Create a profile on user signup. """
+
     if created:
         profile_model = get_profile_model()
         profile_model.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
-def assign_to_authenticated_users_group(sender, instance, created, *args, **kwargs):
+def assign_to_authenticated_users_group(sender, instance, created,
+                                        *args, **kwargs):
+    """ Assign all newly created users to the group 'authenticated_users'.
+
+    If this group does not exists we create it and give permissions
+    from the AUTHENTICATED_USERS_PERMISSIONS Django setting.
     """
-    Here we assign all newly created users to the group 'authenticated_users'
-    If this group does not exists we create it and give permissions from settings 
-    variable AUTHENTICATED_USERS_PERMISSIONS
-    """
-    # Check if authenticated_user group exists, if not create it and add following perms
+    # Check if authenticated_user group exists,
+    # if not create it and add following perms.
     group, created = Group.objects.get_or_create(name='authenticated_users')
-    
+
     # assign perms to group
     permissions = getattr(settings, 'AUTHENTICATED_USERS_PERMISSIONS')
     print "assigning permisions %s" % (permissions.__str__())
     for permission in permissions:
         assign_perm(permission, group)
+
     # assign user to group
     instance.groups.add(group)
