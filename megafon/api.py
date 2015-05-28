ec3c8f4ed1eb070cadcfad839072e187f73d19bd
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-05-28 10:06:07 +0200

    Fix custom end-point method

--- megafon/api.py
+++ megafon/api.py
@@ -4,6 +4,7 @@ from tastypie.resources import ModelResource
 from tastypie import fields
 
 from .models import Post
+from accounts.models import Profile
 
 from graffiti.api import TaggedItemResource
 from dataserver.authentication import AnonymousApiKeyAuthentication
@@ -35,6 +36,7 @@ class PostResource(ModelResource):
         return [
             url(r"^(?P<resource_name>%s)/questions%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_questions'), name="api_get_questions"),
             url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/answers%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_answers'), name="api_get_answers"),
+            url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/contributors%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_contributors'), name="api_get_contributors"),
         ]
 
     def dehydrate(self, bundle):
@@ -51,6 +53,28 @@ class PostResource(ModelResource):
         self.is_authenticated(request)
         self.throttle_check(request)
 
+        post = self.get_object_list(request).get(id=kwargs['pk'])
+        answers = post.get_children()
+        bundles = []
+
+        for obj in answers:
+            bundle = self.build_bundle(obj=obj, request=request)
+            bundles.append(self.full_dehydrate(bundle, for_list=True))
+
+        return self.create_response(request, {'objects' : bundles})
+
+    def get_contributors(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
         question = self.get_object_list(request).get(id=kwargs['pk'])
-        bundles = [self.full_dehydrate(self.build_bundle(obj=answer, request=request)) for answer in question.get_children()]
-        return self.create_response(request, bundles)
+        contributors = question.get_descendants(include_self=False).values_list('author', flat=True)
+        bundles = []
+
+        for obj in Profile.objects.filter(id__in=contributors):
+            contributor_resource = ProfileResource()
+            bundle = contributor_resource.build_bundle(obj=obj, request=request)
+            bundles.append(contributor_resource.full_dehydrate(bundle, for_list=True))
+
+        return self.create_response(request, {'objects' : bundles})
